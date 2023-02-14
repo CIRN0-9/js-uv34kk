9aed07ad13a843954ca4a6e9d29c1e671e199e5b
CIRN0-9 <94833990+CIRN0-9@users.noreply.github.com>
2023-02-14 17:35:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-uv34kk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-uv34kk)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function isPalindrome(x) {
+  if (x < 0) return false;
+  if (x === 0) return true;
+  let reverNum = '';
+  // while (reverNum * 10 < x) {}
+}
+console.log(isPalindrome(123321), 'res');
